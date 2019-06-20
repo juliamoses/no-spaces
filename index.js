2c1fd13d716db3cf0af38f74aa05284f378f5117
Julia Moses <jslmoses@gmail.com>
2019-06-20 11:00:04 -0400

    throw error added to index

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-module.exports = str => {
-  return str = str.replace(/\s/g,'');  
-}:
+'use strict';
+module.exports = function (str) {
+	if (typeof str !== 'string') {
+		throw new TypeError('no-whitespace expects a string');
+	}
+
+	return str.replace(/[\s\n\r]+/gm, '');
+};
